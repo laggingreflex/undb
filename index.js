c363f69459b8770b13896f712a9aa4012fbc66e9
laggingreflex <laggingreflex@gmail.com>
2018-10-10 05:59:21 +0530

    fix: browser detection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
- * (from debug)
+ * (from debug https://github.com/visionmedia/debug/blob/master/src/index.js)
  */
 
-if (typeof process !== 'undefined' && process.type === 'renderer') {
+if (typeof process === 'undefined' || process.type === 'renderer' || process.browser === true || process.__nwjs) {
   module.exports = require('./browser');
 } else {
   module.exports = require('./node');
